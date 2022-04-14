1f903c3d6c40d87ad44d7a5f296086d5b94bbccf
유혜승 <hyeseung0124@gmail.com>
2022-04-14 15:13:17 +0900

    init

---
+++ backend/godlife/src/main/java/com/ovcors/godlife/GodlifeApplication.java
@@ -0,0 +1,13 @@
+package com.ovcors.godlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodlifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GodlifeApplication.class, args);
+	}
+
+}
