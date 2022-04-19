15ab0e5b1da1f14bb393d8ec819cb4308d608cd9
유혜승 <hyeseung0124@gmail.com>
2022-04-19 16:49:55 +0900

    #S06P31A405-165 [Back] Feat: ✨Spring Security 적용

---
+++ backend/godlife/src/main/java/com/ovcors/godlife/config/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,47 @@
+package com.ovcors.godlife.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException e) throws IOException, ServletException {
+        log.error("UnAuthorized error : {}", e.getMessage());
+        String exception = (String) request.getAttribute("exception");
+
+        // Todo: errorCode 만들어지면 주석 제거하기
+//        if("token expired".equals(exception)) {
+//            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+//            response.setStatus(ErrorCode.EXPIRED_TOKEN.getHttpStatus().value());
+//            response.getWriter().println("{ \"message\" : \"" + exception
+//                    + "\", \"code\" : \"" + ErrorCode.EXPIRED_TOKEN.getHttpStatus().value()
+//                    + "\", \"status\" : " + ErrorCode.EXPIRED_TOKEN.getDetail()
+//                    + ", \"errors\" : [ ] }");
+//        }
+//        // response에 넣기
+//        else {
+//            setResponse(response, exception);
+//        }
+    }
+
+    private void setResponse(HttpServletResponse response, String exception) throws IOException {
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.getWriter().println("{ \"message\" : \"" + exception
+                + "\", \"code\" : \"" + HttpStatus.UNAUTHORIZED.value()
+                + "\", \"status\" : " + HttpStatus.UNAUTHORIZED.name()
+                + ", \"errors\" : [ ] }");
+    }
+}
