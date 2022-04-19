3afb96f3ac13bec3b6f38769732ce1430b3b1b6e
유혜승 <hyeseung0124@gmail.com>
2022-04-19 16:51:54 +0900

    #S06P31A405-166 [Back] Feat: ✨JWT 적용

---
+++ backend/godlife/src/main/java/com/ovcors/godlife/config/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,103 @@
+package com.ovcors.godlife.config.jwt;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ovcors.godlife.api.dto.request.LoginReqDto;
+import com.ovcors.godlife.config.auth.PrincipalDetails;
+import com.ovcors.godlife.core.repository.UserRepository;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+
+public class JwtAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+
+    private final AuthenticationManager authenticationManager;
+    private final String secret;
+    private UserRepository userRepository;
+
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager, String secret, UserRepository userRepository) {
+        super("/user/login");
+        this.authenticationManager = authenticationManager;
+        this.secret = secret;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
+        try {
+            ObjectMapper om = new ObjectMapper();
+            LoginReqDto loginReqDto = om.readValue(request.getInputStream(), LoginReqDto.class);
+
+            UsernamePasswordAuthenticationToken authenticationToken =
+                    new UsernamePasswordAuthenticationToken(loginReqDto.getEmail(), loginReqDto.getPassword());
+
+            System.out.println(loginReqDto.getEmail()+" "+loginReqDto.getPassword());
+            Authentication authentication = authenticationManager.authenticate(authenticationToken); // 여기서 PrincipalDetailsService 진입
+
+//            PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+
+            return authentication;
+        } catch (BadCredentialsException e) {
+            sendErrorResponse(response, "BadCredentialException");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private void sendErrorResponse(HttpServletResponse response, String message) throws IOException {
+        System.out.println("잘못된 로그인 정보입니다.");
+        // Todo: ErrorCode만들어지면 주석 해제
+//        response.setCharacterEncoding("UTF-8");
+//        response.setStatus(ErrorCode.USER_NOT_FOUND.getHttpStatus().value());
+//        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+//        response.getWriter().println("{ \"message\" : \"" + message
+//                + "\", \"code\" : \"" + ErrorCode.USER_NOT_FOUND.getHttpStatus().value()
+//                + "\", \"status\" : " + ErrorCode.USER_NOT_FOUND.getHttpStatus().name()
+//                + ", \"errors\" : [ ] }");
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+
+        PrincipalDetails principalDetails = (PrincipalDetails) authResult.getPrincipal();
+
+        // Access Token 발급
+        String jwtToken = JWT.create()
+                .withSubject(principalDetails.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + JwtProperties.EXPIRATION_TIME))
+                .withClaim("id", principalDetails.getUser().getSeq())
+                .withClaim("email", principalDetails.getUser().getEmail())
+                .sign(Algorithm.HMAC512(secret));
+
+        // Refresh Token 발급
+        String refreshToken = JWT.create()
+                .withSubject(principalDetails.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + JwtProperties.REFRESH_EXPIRATION_TIME))
+                .withClaim("email", principalDetails.getUser().getEmail())
+                .sign(Algorithm.HMAC512(secret));
+
+        // Todo : Refresh Token - Redis에 저장하는걸로 바꾸기
+//        User userEntity = userRepository.findByEmailAndActivateTrue(principalDetails.getUsername());
+//        userEntity.saveRefreshToken(refreshToken);
+//        userRepository.save(userEntity);
+
+        System.out.println("success!! -> " + jwtToken);
+        // response의 header로 Access Token 보내기
+        response.addHeader(JwtProperties.HEADER_STRING, JwtProperties.TOKEN_PREFIX + jwtToken);
+        // response의 header로 Refresh Token 보내기
+        response.addHeader(JwtProperties.REFRESH_TOKEN_HEADER_STRING, JwtProperties.TOKEN_PREFIX + refreshToken);
+    }
+}
---
+++ backend/godlife/src/main/java/com/ovcors/godlife/config/jwt/JwtAuthorizationFilter.java
@@ -0,0 +1,68 @@
+package com.ovcors.godlife.config.jwt;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.ovcors.godlife.config.auth.PrincipalDetails;
+import com.ovcors.godlife.core.domain.user.User;
+import com.ovcors.godlife.core.repository.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
+
+    private UserRepository userRepository;
+    private String secret;
+
+    public JwtAuthorizationFilter(AuthenticationManager authenticationManager, UserRepository userRepository, String secret) {
+        super(authenticationManager);
+        this.userRepository = userRepository;
+        this.secret = secret;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String header = request.getHeader(JwtProperties.HEADER_STRING);
+
+        if(header==null || !header.startsWith(JwtProperties.TOKEN_PREFIX)) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        try {
+            String token = request.getHeader(JwtProperties.HEADER_STRING).replace(JwtProperties.TOKEN_PREFIX, "");
+            String email = JWT.require(Algorithm.HMAC512(secret)).build().verify(token)
+                    .getClaim("email").asString();
+
+            if(email != null) {
+                System.out.println(email+" -> 로그인 시도");
+                User user = userRepository.findByEmailAndDeletedFalse(email);
+
+                if(user == null) {
+                    System.out.println("해당하는 유저가 없습니다.");
+                }
+                PrincipalDetails principalDetails = new PrincipalDetails(user);
+
+                Authentication authentication = new UsernamePasswordAuthenticationToken(principalDetails, null, principalDetails.getAuthorities());
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        } catch (TokenExpiredException e) {
+            request.setAttribute("exception", "token expired");
+        } catch(JWTDecodeException e) {
+            request.setAttribute("exception", "wrong token");
+        }
+        chain.doFilter(request, response);
+    }
+}
