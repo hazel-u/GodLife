03d96d7c756222175c0e9f9e45c472e60dc511b3
kang su hyun <kangsu02@gmail.com>
2022-04-19 16:41:27 +0900

    FEAT: ✨ 회원가입, 로그인 폼 타입 설정

---
+++ frontend/godlife/src/types/user.ts
@@ -0,0 +1,11 @@
+export interface JoinInput {
+  email: string;
+  nickname: string;
+  password: string;
+  confirmPassword: string;
+}
+
+export interface LoginInput {
+  email: string;
+  password: string;
+}
