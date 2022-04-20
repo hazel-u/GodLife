a8b00525b14a3c723ccb4e8acf49e0af59feb24b
jeongum <aldlswjddma@naver.com>
2022-04-20 19:48:10 +0900

    #S06P31A405-148 FEAT: ✨ Bingo Service 작성 및 테스트

--- backend/godlife/src/test/java/com/ovcors/godlife/core/repository/BingoRepositoryTest.java
+++ backend/godlife/src/test/java/com/ovcors/godlife/core/repository/BingoRepositoryTest.java
@@ -3,9 +3,14 @@ package com.ovcors.godlife.core.repository;
 import com.ovcors.godlife.api.exception.CustomException;
 import com.ovcors.godlife.api.exception.ErrorCode;
 import com.ovcors.godlife.core.domain.bingo.Bingo;
+import com.ovcors.godlife.core.domain.bingo.Comment;
+import com.ovcors.godlife.core.domain.user.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import javax.persistence.EntityManager;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,6 +19,12 @@ class BingoRepositoryTest {
 
     @Autowired
     private BingoRepository bingoRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private EntityManager em;
 
     @Test
     void createBingo() {
@@ -44,5 +55,51 @@ class BingoRepositoryTest {
 
         // then
         assertThat(findBingo.getTitle()).isEqualTo(savedBingo.getTitle());
+    }
+
+    @Test
+    void findAllBingo() {
+        // given
+        User user = User.builder()
+                .name("정음")
+                .build();
+        userRepository.save(user);
+        Bingo bingo = Bingo.builder()
+                .title("Hello world")
+                .build();
+        bingo.setUser(user);
+        bingoRepository.save(bingo);
+
+        // when
+        List<Bingo> findBingos = bingoRepository.findAllByUser(user);
+
+        // then
+        assertThat(findBingos.size()).isEqualTo(1);
+        assertThat(findBingos.get(0).getTitle()).isEqualTo(bingo.getTitle());
+        assertThat(findBingos.get(0).getUser()).isEqualTo(user);
+    }
+
+    @Test
+    void addComment() {
+        // given
+        Bingo bingo = Bingo.builder()
+                .title("Hello world")
+                .build();
+
+        Bingo savedBingo = bingoRepository.save(bingo);
+
+        // when
+        Comment comment = Comment.builder()
+                .content("comment")
+                .build();
+        savedBingo.addComment(comment);
+        em.flush();
+
+        // then
+        Bingo findBingo = bingoRepository.findById(savedBingo.getSeq())
+                .orElseThrow(()->new CustomException(ErrorCode.BINGO_NOT_FOUND));
+
+        assertThat(findBingo.getComments().size()).isEqualTo(1);
+        assertThat(findBingo.getComments().get(0).getContent()).isEqualTo("comment");
     }
 }
