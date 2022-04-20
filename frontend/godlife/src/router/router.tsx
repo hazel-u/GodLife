9eae49eac77b328905ba7eab82f43b3724403946
kang su hyun <kangsu02@gmail.com>
2022-04-20 16:26:59 +0900

    FEAT: ✨ 로그인 여부에 따른 라우터 제한

--- frontend/godlife/src/router/router.tsx
+++ frontend/godlife/src/router/router.tsx
@@ -1,16 +1,29 @@
-import { useRoutes } from "react-router-dom";
+import { Navigate, Outlet, useRoutes } from "react-router-dom";
 import Join from "../pages/join/Join";
 import Login from "../pages/login/Login";
+import Profile from "../pages/profile/Profile";
 
 export default function Router() {
+  const isAuth = localStorage.getItem("token");
+
   return useRoutes([
     {
       path: "/join",
-      element: <Join />,
+      element: isAuth ? <Navigate to="/" /> : <Join />,
     },
     {
       path: "/login",
-      element: <Login />,
+      element: isAuth ? <Navigate to="/" /> : <Login />,
+    },
+    {
+      path: "/",
+      element: isAuth ? <Outlet /> : <Navigate to="/login" />,
+      children: [
+        {
+          path: "/profile",
+          element: <Profile />,
+        },
+      ],
     },
   ]);
 }
