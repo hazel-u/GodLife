6b36c5fa9464387f7403adcca546193cebfd31d7
soominlee <dltnals7350@naver.com>
2022-04-21 21:32:11 +0900

    CHORE: :robot_face: app.tsx내 import 삭제

--- frontend/godlife/src/App.tsx
+++ frontend/godlife/src/App.tsx
@@ -1,10 +1,8 @@
 import React from "react";
-import Navbar from "./components/bars/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <Navbar menuName={""}/>
     </div>
   );
 
