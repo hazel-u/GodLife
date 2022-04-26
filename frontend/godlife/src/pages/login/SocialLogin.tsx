82dad1f754af2515d54129864f4f69026d439bba
kang su hyun <kangsu02@gmail.com>
2022-04-26 10:37:30 +0900

    STYLE: 🎨 소셜 로그인 버튼 변경

--- frontend/godlife/src/pages/login/SocialLogin.tsx
+++ frontend/godlife/src/pages/login/SocialLogin.tsx
@@ -1,7 +1,10 @@
-import { Stack } from "@mui/material";
+import { Button, Stack } from "@mui/material";
 
 import React from "react";
 import GoogleLogin from "react-google-login";
+
+import { ReactComponent as GoogleLoginImage } from "../../assets/logo/Brand/oAuth/google/google.svg";
+import { ReactComponent as KakaoLoginImage } from "../../assets/logo/Brand/oAuth/kakao/kakao.svg";
 
 const SocialLogin = () => {
   const responseGoogle = (res: any) => {
@@ -13,13 +16,22 @@ const SocialLogin = () => {
       <GoogleLogin
         clientId={process.env.REACT_APP_GOOGLE_OAUTH_KEY!}
         buttonText="구글로 계속하기"
+        render={(renderProps) => (
+          <Button
+            sx={{ padding: 0 }}
+            onClick={renderProps.onClick}
+            disabled={renderProps.disabled}
+          >
+            <GoogleLoginImage />
+          </Button>
+        )}
         onSuccess={responseGoogle}
         onFailure={responseGoogle}
       />
-      <img
-        src={require("../../assets/logo/Brand/oAuth/kakao/kakao_login_medium_narrow.png")}
-        alt="kakao login"
-      />
+
+      <Button sx={{ padding: 0 }}>
+        <KakaoLoginImage />
+      </Button>
     </Stack>
   );
 };
