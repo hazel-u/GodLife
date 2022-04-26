adb55ddf6af77cd15956b2ef0cff2849d73eb165
jeongum <aldlswjddma@naver.com>
2022-04-26 14:10:33 +0900

    #S06P31A405-180 FEAT: ✨ 배포 환경 설정

--- backend/godlife/src/main/java/com/ovcors/godlife/GodlifeApplication.java
+++ backend/godlife/src/main/java/com/ovcors/godlife/GodlifeApplication.java
@@ -2,12 +2,15 @@ package com.ovcors.godlife;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class GodlifeApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(GodlifeApplication.class, args);
+		SpringApplication app = new SpringApplication(GodlifeApplication.class);
+		app.addListeners(new ApplicationPidFileWriter());
+		app.run(args);
 	}
 }
