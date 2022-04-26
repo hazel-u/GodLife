2f8274d557dba7b73fdd40fafc79eaad4622fe4f
soominlee <dltnals7350@naver.com>
2022-04-27 02:42:22 +0900

    FEAT: ✨  즐겨찾기 조회 useEffect 분리

--- frontend/godlife/src/pages/bingo/create/BingoCreateGoalList.tsx
+++ frontend/godlife/src/pages/bingo/create/BingoCreateGoalList.tsx
@@ -34,8 +34,11 @@ const BingoCreateGoalList = () => {
   };
 
   useEffect(() => {
-    getGoals();
     getFavorites();
+  }, [setUserFavorites]);
+
+  useEffect(() => {
+    getGoals();
   }, []);
 
   const category = [
