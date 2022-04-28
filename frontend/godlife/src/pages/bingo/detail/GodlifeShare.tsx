16c3a6467571291451a91f98310f2e15f91febc4
kang su hyun <kangsu02@gmail.com>
2022-04-28 23:32:16 +0900

    FEAT: ✨ 빙고 공유 페이지 로딩

--- frontend/godlife/src/pages/bingo/detail/GodlifeShare.tsx
+++ frontend/godlife/src/pages/bingo/detail/GodlifeShare.tsx
@@ -7,6 +7,7 @@ import { useNavigate, useParams } from "react-router-dom";
 import Bingo from "../../../components/Bingo/Bingo";
 import { selectBingo, setBingo } from "../../../store/bingo";
 import { useAppDispatch, useAppSelector } from "../../../store/hooks";
+import { setLoading } from "../../../store/loading";
 import { selectUser } from "../../../store/user";
 import BingoTitle from "./BingoTitle";
 import CommentList from "./CommentList";
@@ -26,12 +27,16 @@ const GodlifeShare = () => {
       })
       .catch(() => {
         navigate("/404");
+      })
+      .finally(() => {
+        dispatch(setLoading(false));
       });
   }, [params, dispatch, navigate]);
 
   useEffect(() => {
+    dispatch(setLoading(true));
     getBingo();
-  }, [getBingo]);
+  }, [getBingo, dispatch]);
 
   const bingo = useAppSelector(selectBingo);
   const { email } = useAppSelector(selectUser);
@@ -39,37 +44,39 @@ const GodlifeShare = () => {
   return (
     <Stack direction="column" alignItems="center" sx={{ margin: "3%" }}>
       {bingo.code && (
-        <Box sx={{ maxWidth: "500px" }}>
-          {/* 본인의 bingo일 경우에만 실제 id 넘겨주고 그렇지 않다면 빈 문자열 넘기기*/}
-          <BingoTitle
-            id={bingo.userEmail === email ? bingo.id : ""}
-            title={bingo.title}
+        <>
+          <Box sx={{ maxWidth: "500px" }}>
+            {/* 본인의 bingo일 경우에만 실제 id 넘겨주고 그렇지 않다면 빈 문자열 넘기기*/}
+            <BingoTitle
+              id={bingo.userEmail === email ? bingo.id : ""}
+              title={bingo.title}
+              getBingo={getBingo}
+            />
+            <Box sx={{ marginTop: "5%" }}>
+              <Bingo
+                createdBy={bingo.userName}
+                size={3}
+                goals={bingo.goals}
+                mode={"Active"}
+                startDate={bingo.startDate}
+                getBingo={getBingo}
+                godlife={bingo.godlife}
+                id={bingo.id}
+              />
+            </Box>
+          </Box>
+
+          <Interaction
+            code={bingo.code}
+            likeCnt={bingo.likeCnt}
+            seq={bingo.id}
             getBingo={getBingo}
           />
-          <Box sx={{ marginTop: "5%" }}>
-            <Bingo
-              createdBy={bingo.userName}
-              size={3}
-              goals={bingo.goals}
-              mode={"Active"}
-              startDate={bingo.startDate}
-              getBingo={getBingo}
-              godlife={bingo.godlife}
-              id={bingo.id}
-            />
-          </Box>
-        </Box>
+
+          <Share />
+          <CommentList getBingo={getBingo} />
+        </>
       )}
-
-      <Interaction
-        code={bingo.code}
-        likeCnt={bingo.likeCnt}
-        seq={bingo.id}
-        getBingo={getBingo}
-      />
-
-      <Share />
-      <CommentList getBingo={getBingo} />
     </Stack>
   );
 };
