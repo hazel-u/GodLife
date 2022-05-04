79e43dc4861c71332a799627b198a52e10dea792
kang su hyun <kangsu02@gmail.com>
2022-05-04 10:26:17 +0900

    STYLE: 🎨 공유네비바 배경색 삭제

--- frontend/godlife/src/components/bars/ShareNavbar.tsx
+++ frontend/godlife/src/components/bars/ShareNavbar.tsx
@@ -6,7 +6,6 @@ import { useNavigate } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/logo/Godlife/logo.svg";
 
 const DivStyle = styled.div`
-  background-color: #ffffff;
   display: flex;
   justify-content: center;
   align-items: center;
