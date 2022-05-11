1896b0721161271bc111d79f3d0111cac36ebc81
kang su hyun <kangsu02@gmail.com>
2022-05-11 14:49:59 +0900

    #S06P31A405-360 REFACTOR: 💡 패키지 크기 축소

--- frontend/godlife/src/utils/uploadImage.ts
+++ frontend/godlife/src/utils/uploadImage.ts
@@ -1,18 +1,15 @@
-import AWS from "aws-sdk";
+import S3 from "aws-sdk/clients/s3";
 
 const ACCESS_KEY = process.env.REACT_APP_S3_ACCESS_KEY;
 const SECRET_ACCESS_KEY = process.env.REACT_APP_S3_SECRET_ACCESS_KEY;
 const REGION = "ap-northeast-2";
 const S3_BUCKET = "today.godlife";
 
-AWS.config.update({
-  accessKeyId: ACCESS_KEY,
-  secretAccessKey: SECRET_ACCESS_KEY,
-});
-
-const myBucket = new AWS.S3({
+const myBucket = new S3({
   params: { Bucket: S3_BUCKET },
   region: REGION,
+  accessKeyId: ACCESS_KEY,
+  secretAccessKey: SECRET_ACCESS_KEY,
 });
 
 export const uploadImage = (file: any) => {
